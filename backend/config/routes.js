5eb14627524d2f3e2b180680d030676f1e359fbe
Markus Eiglsperger <meiglspe@htwg-konstanz.de>
2024-12-17 22:15:56 +0100

    Fixed production problems.

--- backend/config/routes.js
+++ backend/config/routes.js
@@ -18,6 +18,7 @@ module.exports.routes = {
   * `assets` directory)                                                      *
   *                                                                          *
   ***************************************************************************/
+  '/': { view: 'pages/homepage' },
 
   'POST /login': "login.login",
   'POST /register': "login.register",
--- backend/tasks/register/prod.js
+++ backend/tasks/register/prod.js
@@ -14,7 +14,7 @@
 module.exports = function(grunt) {
   grunt.registerTask('prod', [
     // 'polyfill:dev', //« uncomment to ALSO transpile during development (for broader browser compat.)
-    //'compileAssets',
+    'compileAssets',
     // 'babel',        //« uncomment to ALSO transpile during development (for broader browser compat.)
     'linkAssets',
     //'watch'
